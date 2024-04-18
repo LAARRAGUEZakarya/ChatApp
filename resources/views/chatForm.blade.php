ede9ff6d752c4df92b13bd2e2d70725c4e46ddfc
zkaria <zakariya.laarrague@outlook.fr>
2024-04-18 11:58:27 +0100

    add script of pusher to view and separate files

--- resources/views/chatForm.blade.php
+++ resources/views/chatForm.blade.php
@@ -133,62 +133,6 @@
     </div>
 </div>
 
-<script>
-    $(document).ready(function(){
-        $("#chat_area").scrollTop($("#chat_area")[0].scrollHeight);
-    $('#action_menu_btn').click(function(){
-        $('.action_menu').toggle();
-    });
-        });
-
-
-    $('#send').click(function(){
-        // Retrieve the CSRF token value from a meta tag
-var csrfToken = $('meta[name="csrf-token"]').attr('content');
-        $.post("/sendMessage",
-        {
-            _token: csrfToken, // Include the CSRF token
-            user_id:{{$receiver->id}},
-            message: $("#message").val(),
-        },
-        function(data, status) {
-    // console.log("Data: " + data + "\nStatus: " + status);
-    let senderMessage = `
-        <div class="d-flex justify-content-end mb-4">
-            <div class="msg_cotainer_send">
-                ${$("#message").val()}
-                <span class="msg_time_send">${getCurrentTimeFormatted()}</span>
-            </div>
-            <div class="img_cont_msg">
-                <img src="{{ Auth::user()->image }}" class="rounded-circle user_img_msg">
-            </div>
-        </div>`;
-    $("#chat_area").append(senderMessage);
-    $("#chat_area").scrollTop($("#chat_area")[0].scrollHeight);
-
-    $("#message").val('');
-}
-
-    )
-    })
-
-
-    function getCurrentTimeFormatted() {
-    const currentDate = new Date();
-    let hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-
-    // Convert hours to 12-hour format
-    hours = hours % 12;
-    hours = hours ? hours : 12; // '0' should be '12'
-
-    // Pad minutes with leading zero if necessary
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-
-    return hours + ':' + minutes + ' ' + ampm + ', Today';
-}
-
-</script>
+@include('chatFormScript.ChatFormJs')
 
 @endsection
